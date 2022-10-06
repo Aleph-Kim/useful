6c38ad4f19b839614a88793d12554b09b8ece875
kimchaemin000617 <dktjdej25@gmail.com>
2022-10-06 10:45:38 +0900

    이미지 저장

--- php/basic_php.php
+++ php/basic_php.php
@@ -33,9 +33,41 @@ class basic_php
                 ');
                 break;
         }
-        
+
         return $this->from('테이블이름')
             ->order_by($order_by)
             ->fetch($where, $limit, $option);
+    }
+
+    /**
+     * 입력 이미지를 저장 후 출력 경로를 반환하는 함수
+     * @param string $img base64 인코딩 된 이미지
+     * @return string 출력 경로
+     */
+    function save_img($img)
+    {
+        // 이미지 저장 경로
+        $tempPath = ATTACH_PATH . 'upload/';
+        // 이미지 출력 시 경로
+        $tempURL = ATTACH_URL . 'upload/';
+
+        // 이미지 저장 경로에 디렉토리가 존재하지 않는다면 생성
+        if (!is_dir($tempPath)) {
+            mkdir($tempPath, TRUE);
+            chmod($tempPath, 0755);
+        }
+
+        $explodArray = explode("base64,", $img);
+        $img = $explodArray[1];
+        $img = str_replace(' ', '+', $img);
+        $data = base64_decode($img);
+
+        if ($data === false) {
+            throw new \Exception('base64_decode failed');
+        }
+
+        $pull_path = $tempPath . md5($_SERVER['REMOTE_ADDR']) . '_' . fn_hash_id(TRUE) . '.jpg';
+        file_put_contents($pull_path, $data);
+        return str_replace($tempPath, $tempURL, $pull_path);
     }
 }
