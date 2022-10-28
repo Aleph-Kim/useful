7e4dd2b0d104ea857035906471943957956dc4b4
kimchaemin000617 <dktjdej25@gmail.com>
2022-10-28 20:04:36 +0900

    sql - varchar 자료형인 칼럼 숫자 기준 order_by (group by 시)

--- sql/useful.sql
+++ sql/useful.sql
@@ -1,3 +1,9 @@
 -- varchar 자료형인 칼럼 숫자 기준 order_by
 -- Ex) 전 : 1, 10, 2 / 후 : 1, 2, 10
-ORDER BY ABS(varchar_int);
+ORDER BY ABS(varchar_int)
+
+-- Ex) group by 시
+SELECT ABS(e.varchar_int) as varchar_int
+FROM ex_table e
+GROUP BY e.varchar_int
+ORDER BY varchar_int
