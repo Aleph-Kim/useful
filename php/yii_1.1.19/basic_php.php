39359f075f555b72e76bb2d8594ec4d130b62abc
kimchaemin000617 <dktjdej25@gmail.com>
2023-02-23 14:40:18 +0900

    yii - 객체를 HTMl 형식의 문자열로 출력하는 함수

--- php/yii_1.1.19/basic_php.php
+++ php/yii_1.1.19/basic_php.php
@@ -1,4 +1,15 @@
 <?php
 class basic_php
 {
+    /**
+     * 객체를 HTML 형식의 문자열로 출력하는 함수
+     * @param object $obj 출력할 오브젝트 객체
+     * @param int $depth 출력할 데이터 깊이
+     * @return string HTML 형식으로 출력된 문자열
+     */
+    function dumpObject($obj, $depth = 999) {
+        Yii::import('system.utils.CVarDumper');
+        echo CVarDumper::dumpAsString($obj, $depth, true);
+        exit;
+    }
 }
