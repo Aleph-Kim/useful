f2cc35e1e31e01cc4f7425d66e890928830000fe
kimchaemin000617 <dktjdej25@gmail.com>
2023-05-24 10:38:54 +0900

    php - 휴대폰 번호 / id / 이름 마스킹 함수 추가

--- php/useful/masking.php
+++ php/useful/masking.php
@@ -1,4 +1,72 @@
-<?php
+<?
 class Masking
 {
+    /**
+     * 휴대폰 번호 마스킹
+     */
+    function phoneNoMasking($str)
+    {
+        $str = str_replace('-', '', $str);
+        $strlen = mb_strlen($str, 'utf-8');
+        $mValue = "";
+
+        switch ($strlen) {
+            case 10:
+                $mValue = mb_substr($str, 0, 3) . "-" . mb_substr($str, 3, 1) . "**" . "-*" . mb_substr($str, 7, 3);;
+                break;
+            case 11:
+                $mValue = mb_substr($str, 0, 3) . "-" . mb_substr($str, 3, 2) . "**" . "-*" . mb_substr($str, 8, 3);
+                break;
+            case 0:
+                $mValue = '';
+                break;
+        }
+        return $mValue;
+    }
+
+    /**
+     * id 마스킹
+     */
+    function IDMasking($str)
+    {
+        $mValue = preg_replace('/.{3}$/', '***', $str);
+        return $mValue;
+    }
+
+    /**
+     * IP 마스킹
+     */
+    function IPaddressMasking($str)
+    {
+        // ? : 0 ~ 1, + : 1개 이상
+        $str = preg_replace('/(\d+)([\.]\d+[\.])(\d+)([\.]\d+)/i', '***\2***\4', $str);
+        return $str;
+    }
+
+    /**
+     * 이름 마스킹
+     */
+    function letterMasking($str)
+    {
+        $str = str_replace('-', '', $str);
+        $strlen = mb_strlen($str, 'utf-8');
+        $mValue = "";
+
+        switch ($strlen) {
+            case 2:
+                $mValue = mb_strcut($str, 0, 3, "UTF-8") . '*';
+                break;
+            case 3:
+                $mValue = mb_strcut($str, 0, 3, "UTF-8") . '*' . mb_strcut($str, 8, 11, "UTF-8");
+                break;
+            case 4:
+                $mValue = mb_strcut($str, 0, 3, "UTF-8") . '**' . mb_strcut($str, 12, 15, "UTF-8");
+                break;
+            default:
+                $mValue = mb_strcut($str, 0, 3, "UTF-8") . '**' . mb_strcut($str, 12, 15, "UTF-8");
+                break;
+        }
+        return $mValue;
+    }
+    // 출처 - https://link2me.tistory.com/2027
 }
