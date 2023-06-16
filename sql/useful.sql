9b2c18673aa247fe3ade5e05610123281995fbb8
kimchaemin000617 <dktjdej25@gmail.com>
2023-06-16 11:08:16 +0900

    mysql - 테이블 복사 추가

--- sql/useful.sql
+++ sql/useful.sql
@@ -7,3 +7,9 @@ SELECT ABS(e.varchar_int) as varchar_int
 FROM ex_table e
 GROUP BY e.varchar_int
 ORDER BY varchar_int
+
+-- 테이블 구조 복사
+CREATE TABLE IF NOT EXISTS `new_table` LIKE `old_table`;
+
+-- 테이블 구조 + 데이터 복사
+CREATE TABLE IF NOT EXISTS `new_table` SELECT * FROM `old_table`;
