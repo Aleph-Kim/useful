6e700504f3fc54584c4e3f57341c843438c1023a
kimchaemin000617 <dktjdej25@gmail.com>
2023-08-04 17:02:44 +0900

    mysql - 쿼리 가독성을 위한 수정

--- sql/useful.sql
+++ sql/useful.sql
@@ -3,10 +3,14 @@
 ORDER BY ABS(varchar_int)
 
 -- Ex) group by 시
-SELECT ABS(e.varchar_int) as varchar_int
-FROM ex_table e
-GROUP BY e.varchar_int
-ORDER BY varchar_int
+SELECT 
+    ABS(e.varchar_int) as varchar_int
+FROM 
+    ex_table e
+GROUP BY 
+    e.varchar_int
+ORDER BY 
+    varchar_int
 
 -- 테이블 구조 복사
 CREATE TABLE IF NOT EXISTS `new_table` LIKE `old_table`;
@@ -15,13 +19,19 @@ CREATE TABLE IF NOT EXISTS `new_table` LIKE `old_table`;
 CREATE TABLE IF NOT EXISTS `new_table` SELECT * FROM `old_table`;
 
 -- 정규식 조건문
-SELECT *
-FROM 테이블명
-WHERE 칼럼명 REGEXP '정규식조건';
+SELECT 
+    *
+FROM 
+    테이블명
+WHERE 
+    칼럼명 REGEXP '정규식조건';
 -- EX) 1, 2, 3으로 시작하는 값만 가져오는 조건
-SELECT *
-FROM 테이블명
-WHERE 칼럼명 REGEXP '^1|^2|^3';
+SELECT 
+    *
+FROM 
+    테이블명
+WHERE 
+    칼럼명 REGEXP '^1|^2|^3';
 
 -- 정규식 리스트
 . : 문자 하나
