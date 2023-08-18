b962e2b5529b48f4baeadc52664f9860f7c0d19a
kimchaemin000617 <dktjdej25@gmail.com>
2023-08-18 18:17:36 +0900

    php - curl 통신 기본 함수

--- php/useful/useful_function.php
+++ php/useful/useful_function.php
@@ -39,4 +39,34 @@ class Useful_function
         echo '</pre>';
         exit;
     }
+
+    /**
+     * curl 통신 호출 함수
+     *
+     * @param  String $url - 통신 url
+     * @param  String|Null $method - 통신 타입
+     * @param  Array|Null $params - 파라미터
+     * 
+     * @return String - 통신 결과
+     */
+    public static function curlCall($url, $method = 'POST', $params)
+    {
+        $method = ($method == 'POST') ? 1 : 0;  // TRUE : POST, FALSE : GET
+
+        $ch = curl_init(); // cURL 세션 초기화
+        curl_setopt($ch, CURLOPT_URL, $url); // 요청할 URL 설정
+        curl_setopt($ch, CURLOPT_POST, $method); // HTTP POST 메서드를 사용하여 요청 설정
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params)); // POST 데이터를 전달할 파라미터를 URL 인코딩하여 설정
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // 응답 데이터를 반환받기 위해 true로 설정 (false일 경우 직접 출력)
+        curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4); // IP 주소 해결 방식을 IPv4로 설정 (IPv6 대응, 해제 시 통신 속도 저하)
+        $result = curl_exec($ch); // cURL 요청을 실행하고 응답 데이터 받기
+        curl_close($ch); // cURL 세션 닫기
+        
+        return $result;
+
+
+        // 추가 옵션
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // SSL 인증서 검증 비활성화 (보안 연결 설정)
+        curl_setopt($ch, CURLOPT_TIMEOUT_MS, 10); // 10ms 뒤에 통신 종료(반환값을 기다리지 않음)
+    }
 }
