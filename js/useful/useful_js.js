bad9dc8a77df10143623c40767e0ef38425b3911
김채민 <dktjdej25@gmail.com>
2025-03-31 12:59:47 +0900

    js - 한글 문자열에서 초성, 중성, 종성을 추출하는 함수

--- js/useful/useful_js.js
+++ js/useful/useful_js.js
@@ -230,7 +230,71 @@ form.addEventListener('submit', (event) => {
  * @returns {boolean} - 모바일 여부
  */
 function isMobile() {
-	return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+/**
+ * 한글 문자열에서 초성, 중성, 종성을 추출하는 함수
+ * 
+ * @param {string} str - 변환할 문자열
+ * @returns {string[]} - 초성, 중성, 종성이 분리된 배열
+ */
+function separateKoreanCharacters(str) {
+    // 유니코드 한글 시작 코드: '가'의 유니코드 값
+    const HANGUL_START = 0xAC00;
+    // 유니코드 한글 끝 코드: '힣'의 유니코드 값 
+    const HANGUL_END = 0xD7A3;
+    // 한글 중성(모음)의 개수: 'ㅏ'부터 'ㅣ'까지 총 21개
+    const JUNGSUNG_COUNT = 21;
+    // 한글 종성(받침)의 개수: 받침 없음 포함 총 28개
+    const JONGSUNG_COUNT = 28;
+
+    // 한글 초성 배열: 'ㄱ'부터 'ㅎ'까지 총 19개의 자음
+    const CHOSUNG = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+    // 한글 중성 배열: 'ㅏ'부터 'ㅣ'까지 총 21개의 모음
+    const JUNGSUNG = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'];
+    // 한글 종성 배열: 받침 없음('')부터 'ㅎ'까지 총 28개의 받침
+    const JONGSUNG = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+
+    // 한글 전체(자음, 모음, 완성형 한글) 매칭 패턴
+    const HANGUL_PATTERN = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g;
+    // 자음만 매칭하는 패턴
+    const CONSONANT_PATTERN = /[ㄱ-ㅎ]/;
+    // 모음만 매칭하는 패턴
+    const VOWEL_PATTERN = /[ㅏ-ㅣ]/;
+
+    // 결과 문자열을 배열로 구성하여 join으로 최종 결합
+    let result = [];
+
+    str.replace(HANGUL_PATTERN, (char) => {
+        const code = char.charCodeAt(0);
+
+        // 완성형 한글인 경우
+        if (code >= HANGUL_START && code <= HANGUL_END) {
+            const normalized = code - HANGUL_START;
+            const chosungIndex = Math.floor(normalized / (JUNGSUNG_COUNT * JONGSUNG_COUNT));
+            const jungsungIndex = Math.floor((normalized % (JUNGSUNG_COUNT * JONGSUNG_COUNT)) / JONGSUNG_COUNT);
+            const jongsungIndex = normalized % JONGSUNG_COUNT;
+
+            result.push(
+                CHOSUNG[chosungIndex],
+                JUNGSUNG[jungsungIndex],
+                JONGSUNG[jongsungIndex]
+            );
+            return;
+        }
+
+        // 자음 또는 모음만 있는 경우
+        if (CONSONANT_PATTERN.test(char) || VOWEL_PATTERN.test(char)) {
+            result.push(char);
+            return;
+        }
+
+        // 그 외 문자
+        result.push(char);
+    });
+
+    return result;
 }
 
 // 페이지 로드 시  함수 실행
